9361772ca90cdf24d3e7b2338b3940ca40949942
Ivan Kalita <kaduev13@gmail.com>
2017-04-08 21:48:12 +0300

    Add PeclEvLoopTimerTest.

---
+++ tests/Timer/PeclEvLoopTimerTest.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @author Ivan Kalita kaduev13@gmail.com
+ */
+
+namespace React\Tests\EventLoop\Timer;
+
+
+use React\EventLoop\PeclEvLoop;
+
+class PeclEvLoopTimerTest extends AbstractTimerTest
+{
+    public function createLoop()
+    {
+        if (!class_exists('EvLoop')) {
+            $this->markTestSkipped('pecl-ev tests skipped because ext-ev is not installed.');
+        }
+
+        return new PeclEvLoop();
+    }
+}
